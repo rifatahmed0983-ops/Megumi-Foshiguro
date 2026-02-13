e50523135b75acea7cb27a1fd718303c196162c4
rifatahmed0983-ops <rifatahmed0983@gmail.com>
2026-02-14 00:38:51 +0600

    Update adbot.js

--- Script/commands/adbot.js
+++ Script/commands/adbot.js
@@ -54,7 +54,7 @@ module.exports.run = async ({ api, event, args }) => {
     if (args[0] == "admin") {
         let callback = () => api.sendMessage(
             {
-                body: `———»ADMIN BOT«———\n❯ Name: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔\n❯ Facebook: https://facebook.com/100044713412032\n❯ Thanks for using ${global.config.BOTNAME} bot`,
+                body: `———»ADMIN BOT«———\n❯ Name: 𝐗𝐢𝐭 𝐙𝐞𝐟𝐨𝐱\n❯ Facebook: https://www.facebook.com/share/1HtQxDp26t/\n❯ Thanks for using ${global.config.BOTNAME} bot`,
                 attachment: fs.createReadStream(__dirname + "/cache/1.png")
             },
             event.threadID,
