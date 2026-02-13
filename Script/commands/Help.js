192375ecd4fc3078a58549a4fa737dc6920cfdf5
rifatahmed0983-ops <rifatahmed0983@gmail.com>
2026-02-14 01:49:30 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -43,10 +43,10 @@ module.exports.languages = {
 
 // 🔹 এখানে আপনার ফটো Imgur লিংক করে বসাবেন ✅
 const helpImages = [
-    "https://i.imgur.com/sxSn1K3.jpeg",
-    "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/sxSn1K3.jpeg"
+    "",
+    "",
+    "",
+    ""
 ];
 
 
